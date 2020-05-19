ffed617011cfea9530e4744b2b26c1f27544bdbc
imran-ss <iali@saltside.se>
2020-05-19 14:45:59 +0530

    initial commit

---
+++ app/src/main/java/com/imran/dolfil_login_sample/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.imran.dolfil_login_sample
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Handler().postDelayed(Runnable { // This method will be executed once the timer is over
+            val i = Intent(this@MainActivity, SignupScreen::class.java)
+            startActivity(i)
+            finish()
+        }, 5000)
+    }
+}
---
+++ app/src/main/java/com/imran/dolfil_login_sample/SignupScreen.kt
@@ -0,0 +1,12 @@
+package com.imran.dolfil_login_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signu_screen)
+    }
+}
